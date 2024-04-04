57305363359e11c407005c8b80fd35cc28ac619b
jdailey99 <dailejos@oregonstate.edu>
2024-04-03 22:14:44 -0700

    Corrected error in the input prompt

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Joshua Dailey
+# GitHub username: jdailey99
+# Date: 04/03/2024
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
