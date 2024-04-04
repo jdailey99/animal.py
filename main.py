5874b768e0d863c7080f783d3d39b165198266a0
jdailey99 <dailejos@oregonstate.edu>
2024-04-03 22:16:06 -0700

    Edited data in line items above.

---
+++ main.py
@@ -0,0 +1,8 @@
+# Author: Tim Alcon
+# GitHub username: timalcon
+# Date: 1/1/1970
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
